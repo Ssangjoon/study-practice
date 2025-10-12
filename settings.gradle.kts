f0c7fefd2e6b62d97137bcbc7697364488213195
ssang <qkqh34k@gmail.com>
2025-10-12 19:03:06 +0900

    오브젝트 chapter6 :

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,6 +4,7 @@ include("study:object:chapter01")
 include("study:object:chapter02")
 include("study:object:chapter04")
 include("study:object:chapter05")
+include("study:object:chapter06")
 
 include("study:head-first-design-pattern")
 

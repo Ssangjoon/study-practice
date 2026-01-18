13cd4db78e4b59dbeff1f316152463a233c91039
ssang <qkqh34k@gmail.com>
2026-01-18 20:47:08 +0900

    오브젝트 chapter7 : 하향식 기능 문해의 문제점

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,9 +5,11 @@ include("study:object:chapter02")
 include("study:object:chapter04")
 include("study:object:chapter05")
 include("study:object:chapter06")
+include("study:object:chapter07")
 
 include("study:head-first-design-pattern")
 
 include("practice:object:reservationAgency")
 include("practice:spring-transaction")
 include("practice:modern-java")
+include("study:object:chapter07")
